87b6caf96b5d17336762b64d162d2426321c406e
daviddeng78 <ddeng30@stuy.edu>
2022-01-18 13:20:17 -0500

    v1 offline

---
+++ v1/Game.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Game {
+  public static final String RESET = "\u001B[0m";
+  public static final String RED = "\u001B[31m";
+  public static final String GREEN = "\u001B[32m";
+  public static final String YELLOW = "\u001B[33m";
+  public static final String BLUE = "\u001B[34m";
+  public static final String PURPLE = "\u001B[35m";
+  public static final String CYAN = "\u001B[36m";
+  public static void main(String[] args) {
+    System.out.println(RED + "This text is red!" + RESET);
+    System.out.println(BLACK + "This text is red!" + RESET);
+    System.out.println(GREEN + "This text is red!" + RESET);
+    System.out.println(YELLOW + "This text is red!" + RESET);
+    System.out.println(BLUE + "This text is red!" + RESET);
+    System.out.println(PURPLE + "This text is red!" + RESET);
+    System.out.println(CYAN + "This text is red!" + RESET);
+  }
+
+  public static void setup() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Where would you like to place troops? (Enter territory address (e.g. 1A))");
+    int territory = sc.next();
+    if (territory.matches("[1-<>][A-<>]")) {
+      System.out.println("How many troops would you like to place here?");
+      
+    } //update this later when we decide on map size
+  }
+
+}
---
+++ v1/Map.java
@@ -0,0 +1,17 @@
+public class Map {
+  private static final int[][] map = {}
+  public toString(int[][] map) {
+    for (int row = 0; i < map.length; row++) {
+      for (int column = 0; i < map[0].length; column++) {
+        System.out.print(map[row][column] + "\t");
+      }
+      System.out.println();
+    }
+  }
+
+  int[][] map = {};
+
+  public static void main(String[] args) {
+    toString(map);
+  }
+}
