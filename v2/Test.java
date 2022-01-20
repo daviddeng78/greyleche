48f9b02b85210278536db5a1403cfa5d76954522
David Deng <78229116+daviddeng78@users.noreply.github.com>
2022-01-19 22:35:07 -0500

    removed color test

--- v2/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static final String RESET = "\u001B[0m";
-    public static final String RED = "\u001B[31m";
-
-    public static void main(String[] args) {
-        System.out.println(RED + "This is red" + RESET);
-    }
-}
